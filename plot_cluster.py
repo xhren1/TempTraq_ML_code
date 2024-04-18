4c74fd3f062959c90bd7a798d849d124c509c7e4
Ren <xhren@med.umich.edu>
2024-04-18 18:41:10 -0400

    add two files

---
+++ plot_cluster.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+from tslearn.clustering import silhouette_score
+from tslearn.clustering import TimeSeriesKMeans
+import matplotlib.pyplot as plt
+
+
+def data_plot (train_data,label):
+    '''
+    Plot the data before clustering.
+    
+    Args:
+        train_data (np.array): preprocessed data for clustering
+        orignial_percent (list): the percentage of original data
+
+    Returns:
+        None, but plot the data
+    '''
+
+    x = np.arange(-train_data.shape[1],train_data.shape[1],2)
+    for i in range(len(train_data)):
+        
+        plt.subplot(int(len(train_data)/5)+1, 5, i + 1)
+        plt.plot(x,train_data[i].ravel(), "k-", alpha=.5)
+        plt.axhline(y=38, color='red', linestyle='--',lw=0.8)
+        plt.axvline(x=0, color='green', linestyle='--',lw=0.8)
+        plt.xlim(-train_data.shape[1], train_data.shape[1])
+        plt.ylim(35, 41)
+        plt.title(label[i])
+
+
+def cluster_number_decision(train_data, min_clusters=2,max_clusters=9,seed = 10):
+    '''
+    Plot the square distances for different number of clusters.
+    
+    Args:
+        train_data (np.array): preprocessed data for clustering
+        min_clusters (int): minium number of clusters
+        max_clusters (int): maximum number of clusters
+        
+    Returns:
+        None, only plot the square distances for different number of clusters
+    '''
+    squared_distances = []
+    for n_clusters in range(min_clusters,max_clusters+1):
+        km = TimeSeriesKMeans(n_clusters=n_clusters, metric="dtw", n_init=2, max_iter_barycenter=10, verbose=False, random_state=seed)
+        km = km.fit(train_data)
+        squared_distances.append(km.inertia_)
+    plt.plot(range(min_clusters,max_clusters+1),squared_distances,"*-")
+    plt.xlabel("Number of clusters")
+    plt.ylabel("Squared distances")
+    
+    
+def DTW_KMeans_clustering(train_data, cluster_number,seed = 10):
+    '''
+    Cluster the data using DTW KMeans. 
+    Plot the cluster centers and the data for each cluster.
+    Print the silhouette score for the clustering.
+    
+    Args:
+        train_data (np.array): preprocessed data for clustering
+        cluster_number (int): number of clusters
+        seed (int): random seed
+        
+    Returns:
+        prediect_result (np.array): the cluster labels for each data point
+        km.cluster_centers_ (np.array): the cluster centers
+        km (TimeSeriesKMeans): the clustering model
+    '''
+    
+    km = TimeSeriesKMeans(n_clusters=cluster_number,
+                            n_init=2,
+                            metric="dtw",
+                            verbose=False,
+                            max_iter_barycenter=10,
+                            random_state=seed)
+    prediect_result = km.fit_predict(train_data)
+    # print silhouette score
+    print("DBA silhouette: {:.2f}".format(silhouette_score(train_data, prediect_result, metric="dtw")))
+    
+    x = np.arange(-train_data.shape[1],train_data.shape[1],2).reshape(-1,1)
+
+    for i in range(cluster_number):
+        plt.subplot(3, cluster_number, i+1)
+        # plot all the patient temperature curves
+        for j in train_data[prediect_result == i]:
+            plt.plot(x, j.ravel(), "k-", alpha=.25)
+        # plot the cluster center in blue
+        plt.plot(x, km.cluster_centers_[i].ravel(), "blue")
+        plt.xlim(-train_data.shape[1], train_data.shape[1])
+        # set the y axis limit to body temperature range
+        plt.ylim(34, 41)
+        plt.axhline(y=38, color='red', linestyle='--',lw=0.8)
+        plt.axvline(x=0, color='green', linestyle='--',lw=0.8)
+        
+        plt.text(0.55, 0.85,'Cluster %d' % (i + 1),
+                transform=plt.gca().transAxes, size=14)
+
+        ax = plt.gca()
+        ax.tick_params(axis='x', labelsize=16)
+        ax.tick_params(axis='y', labelsize=16)
+
+    plt.tight_layout()
+
+    return prediect_result, km.cluster_centers_, km
---
+++ preprocess.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+def deal_with_missing_points(temp_data_path,fever_start_data_path,cohort):
+    '''
+    This function deals with the missing data points (no nan recored in the file) in the temperature data.
+    The function will fill the missing data points with nan and output the data to a csv file ("./full_ttemp.csv").
+    The function will also record the original data positions in the data.
+
+    Parameters:
+    temp_data_path (str): The path to the temperature data file.
+    fever_start_data_path (str): The path to the fever start data file.
+    cohort (str): The cohort to be analyzed ("HCT"or"CART").
+
+    Returns:
+    final_df (pd.DataFrame): The data with missing points filled with nan and the original data positions recorded.
+    
+    '''
+    # Load the data
+    data = pd.read_csv(temp_data_path, sep=',')
+
+    data = data[data['Cohort'] == cohort]
+    data = data=data[["MaskID","Time_DPI","TTemp"]]
+    data["fever_start"] = 0
+    data["Time_DPI"] = data["Time_DPI"]*24*60
+    
+    # make sure all the time points are even
+    data["Time_DPI"] = data["Time_DPI"].round(0).apply(lambda x: x if x % 2 == 0 else x+1)
+    
+    # Load the data
+    fever_start = pd.read_csv(fever_start_data_path, sep=',')
+    
+    fever_start = fever_start[fever_start['Cohort'] == cohort]
+    fever_start = fever_start[["MaskID","Time_DPI"]]
+    fever_start["Time_DPI"] = fever_start["Time_DPI"]*24*60
+    
+    # make sure all the time points are even
+    fever_start["Time_DPI"] = fever_start["Time_DPI"].round(0).apply(lambda x: x if x % 2 == 0 else x+1)
+
+    # reset the index
+    data = data.reset_index(drop=True)
+
+    final_df = pd.DataFrame(columns=['MaskID', 'Time_DPI', 'TTemp', 'fever_start'])
+
+    # Loop through each maskid and fill the missing values with nan
+    for maskid in data['MaskID'].unique():
+        print(maskid)
+        time_dpi = data.loc[data['MaskID'] == maskid, 'Time_DPI'].tolist()
+        ttemp = data.loc[data['MaskID'] == maskid, 'TTemp'].tolist()
+        new_index = range(int(time_dpi[0]), int(time_dpi[-1])+2, 2)
+        df = pd.DataFrame({'MaskID': maskid, 'Time_DPI': new_index, 'TTemp': np.nan, 'fever_start': 0, 'Cohort': maskid.split('-')[0]})
+        for i, time in enumerate(tqdm(df["Time_DPI"])):
+            if time in time_dpi:
+                temp_t = ttemp[time_dpi.index(time)]
+                df.iloc[i,2] = temp_t
+        final_df = pd.concat([final_df, df], axis=0)
+        print('-'*80)
+
+    # record the original data positions
+    final_df["Orignial_data"]= ~final_df["TTemp"].isna().astype(int) + 2
+
+    # output the data
+    final_df.to_csv("./full_ttemp.csv", index=False)
+
+    return final_df
+
+if __name__ == "__main__":
+    temp_data_path = "../input/TempTraq_Dataset.csv"
+    fever_start_data_path = "../input/TFeverStarts.csv"
+    cohort = "HCT"
+    temp_df = deal_with_missing_points(temp_data_path,fever_start_data_path,cohort)
+    print("Done!")
